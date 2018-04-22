25d128a7bc59e0a887d9aa538d25963ceb8700c8
yangyijun <1012293987@qq.com>
2018-04-22 21:59:04 +0800

    添加表结构

--- src/main/java/com/app/framework/auth/model/EditPasswordParam.java
+++ src/main/java/com/app/framework/auth/model/EditPasswordParam.java
@@ -1,14 +1,15 @@
 package com.app.framework.auth.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.validator.constraints.NotBlank;
 
 public class EditPasswordParam {
-    @NotBlank
-//    @ApiModelProperty(value = "旧密码", required = true)
+    @NotBlank(message = "旧密码不能为空")
+    @ApiModelProperty(value = "旧密码", required = true)
     private String oldPassword;
 
-    @NotBlank
-//    @ApiModelProperty(value = "新密码", required = true)
+    @NotBlank(message = "新密码不能为空")
+    @ApiModelProperty(value = "新密码", required = true)
     private String newPassword;
 
     public String getOldPassword() {
