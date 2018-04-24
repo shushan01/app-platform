b1b7aa996e2f6884817cd8566c356fa53e545f92
yangyijun <1012293987@qq.com>
2018-04-24 23:16:00 +0800

    去掉多余的代码

--- src/main/java/com/app/framework/auth/controller/AccountController.java
+++ src/main/java/com/app/framework/auth/controller/AccountController.java
@@ -5,9 +5,10 @@ import com.app.framework.auth.model.LoginUser;
 import com.app.framework.auth.service.UserService;
 import com.app.framework.base.BaseController;
 import com.app.framework.core.utils.Md5SaltUtil;
-import com.app.framework.core.utils.PageResult;
 import com.app.framework.core.utils.Response;
 import com.app.framework.core.utils.Status;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UsernamePasswordToken;
@@ -15,7 +16,10 @@ import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.security.NoSuchAlgorithmException;
@@ -28,20 +32,15 @@ public class AccountController extends BaseController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "用户登录接口", notes = "用户登录接口")
     @PostMapping("/login")
     @SuppressWarnings("unchecked")
-    public Response login(@RequestParam(name = "name") String name, @RequestParam(name = "password") String password) {
+    public Response login(@ApiParam(value = "用户名") @RequestParam String name, @ApiParam(value = "密码") @RequestParam String password) {
         SecurityUtils.getSubject().login(new UsernamePasswordToken(name, password));
-//        LoginUser user = SecurityUtils.getSubject().getPrincipals().oneByType(LoginUser.class);
         return new Response(Status.SUCCESS.code(), Status.SUCCESS.msg());
     }
 
-    @RequestMapping("/findAll")
-    @SuppressWarnings("unchecked")
-    public PageResult<LoginUser> findAll(int pageNo, int pageSize) {
-        return userService.findAll(pageNo, pageSize);
-    }
-
+    @ApiOperation(value = "退出登录", notes = "退出登录")
     @PostMapping("/logout")
     public Response logout() {
         Subject subject = SecurityUtils.getSubject();
@@ -52,6 +51,7 @@ public class AccountController extends BaseController {
         return new Response(Status.SUCCESS.code(), Status.SUCCESS.msg());
     }
 
+    @ApiOperation(value = "修改密碼", notes = "修改密碼")
     @PostMapping("/editpwd")
     @RequiresAuthentication
     public Response editPassword(@RequestBody @Valid EditPasswordParam editPasswordParam) throws NoSuchAlgorithmException, InvalidKeySpecException {
--- src/main/java/com/app/framework/dao/AddressMapper.java
+++ src/main/java/com/app/framework/dao/AddressMapper.java
@@ -3,16 +3,5 @@ package com.app.framework.dao;
 import com.app.framework.core.utils.BaseDao;
 import com.app.framework.model.Address;
 
-public interface AddressMapper extends BaseDao {
-    int deleteByPrimaryKey(Long id);
-
-    int insert(Address record);
-
-    int insertSelective(Address record);
-
-    Address selectByPrimaryKey(Long id);
-
-    int updateByPrimaryKeySelective(Address record);
-
-    int updateByPrimaryKey(Address record);
+public interface AddressMapper extends BaseDao<Address> {
 }
