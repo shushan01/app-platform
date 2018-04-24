eca388d11223366313e1f485e5d155629f7a249c
yangyijun <1012293987@qq.com>
2018-04-24 23:44:11 +0800

    添加swagger api配置

--- src/main/java/com/app/framework/core/utils/Response.java
+++ src/main/java/com/app/framework/core/utils/Response.java
@@ -1,10 +1,14 @@
 package com.app.framework.core.utils;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * Created by yangyijun on 2018/4/13.
  */
 public class Response {
+    @ApiModelProperty(value = "响应编码")
     public int code;
+    @ApiModelProperty(value = "响应消息")
     public String msg;
 
     public Response(int code, String msg) {
--- src/main/java/com/app/framework/core/utils/ResponseData.java
+++ src/main/java/com/app/framework/core/utils/ResponseData.java
@@ -1,9 +1,12 @@
 package com.app.framework.core.utils;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * Created by yangyijun on 2018/4/13.
  */
 public final class ResponseData<T> extends Response {
+    @ApiModelProperty(value = "响应数据")
     private T data;
 
     public T getData() {
