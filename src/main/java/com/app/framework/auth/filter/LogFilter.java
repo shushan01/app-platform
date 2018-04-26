bb1de68c5c51dea3c334546fb70735a9770bee8d
yangyijun <1012293987@qq.com>
2018-04-26 22:41:14 +0800

    添加请求过滤器

---
+++ src/main/java/com/app/framework/auth/filter/LogFilter.java
@@ -0,0 +1,25 @@
+package com.app.framework.auth.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*", filterName = "logFilter", dispatcherTypes = DispatcherType.REQUEST)
+public class LogFilter implements Filter {
+    private static final String DEFAULT_ENCODING = "UTF-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
